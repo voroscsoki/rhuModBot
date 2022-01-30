631af3d1557c0be123db70c6456284f3b76afde0
voroscsoki <wcsongor02@gmail.com>
2022-01-30 06:38:00 +0100

    Add database migration and functions

---
+++ Logging.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rhuModBot
+{
+    public class PostContext : DbContext
+    {
+        public DbSet<DbPost> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=posts.db");
+    }
+    public class DbPost
+    {
+        public DbPost(string id, DateTime time, string linkedArticle, string postTitle, string articleTitle, double similarity, bool userIsIgnored, bool domainIsIgnored, bool isReported)
+        {
+            this.id = id;
+            this.time = time;
+            this.linkedArticle = linkedArticle;
+            this.postTitle = postTitle;
+            this.articleTitle = articleTitle;
+            this.similarity = similarity;
+            this.userIsIgnored = userIsIgnored;
+            this.domainIsIgnored = domainIsIgnored;
+            this.isReported = isReported;
+        }
+        [Key]
+        public int key { get; set; }
+        public string id { get; set; }
+        public DateTime time { get; set; }
+        public string? linkedArticle { get; set; }
+        public string postTitle { get; set; }
+        public string? articleTitle { get; set; }
+        public double similarity { get; set; }
+        public bool userIsIgnored { get; set; }
+        public bool domainIsIgnored { get; set; }
+        public bool isReported { get; set; }
+    }
+    public static class DbOperations
+    {
+        public static bool entryExistsInDB(DbPost test)
+        {
+            using (var db = new PostContext())
+            {
+                return db.Posts.Where(p => p.id == test.id).Count() > 0;
+            }
+        }
+        public static void addPost(DbPost toAdd)
+        {
+            using (var db = new PostContext())
+            {
+                if (!entryExistsInDB(toAdd))
+                    db.Posts.Add(toAdd);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Migrations/20220130052152_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace rhuModBot.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    key = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    id = table.Column<string>(type: "TEXT", nullable: true),
+                    time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    linkedArticle = table.Column<string>(type: "TEXT", nullable: true),
+                    postTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    articleTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    similarity = table.Column<double>(type: "REAL", nullable: false),
+                    userIsIgnored = table.Column<bool>(type: "INTEGER", nullable: false),
+                    domainIsIgnored = table.Column<bool>(type: "INTEGER", nullable: false),
+                    isReported = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.key);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
